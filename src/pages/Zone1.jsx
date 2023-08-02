df31f52f8dcd23665a70f9add157583ae7ad8758
kelynblock <kelynblock@gmail.com>
2023-08-01 21:16:08 -0400

    4th commit

--- src/pages/Zone1.jsx
+++ src/pages/Zone1.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Bron from "../pics/Bron.jpg";
+import Booker from "../pics/Booker.jpg";
 
 const Zone1 = () => {
   return (
@@ -34,84 +35,86 @@ const Zone1 = () => {
               <li>They'll become better all around players.</li>
               <li> Their basketball IQ should increase.</li>
             </ol>
-            <div className="Cards">
-              <section className="card1">
-                <img src={Bron} />
-                <div className="card1-title">
-                  <h3>Dunk:</h3>
-                  <p className="card1-body"></p>
-                </div>
-                <p>
-                  <ul>
-                    <li>
-                      <b>How:</b> Forcefully put the ball through the hoop off 1
-                      or 2 feet.
-                    </li>
-                    <li>
-                      <b>Why:</b> Is a better option over a typical layup if
-                      able to perform. A more aggressive play. Can create
-                      momentum or fouls depending on the situation.
-                    </li>
-                    <li>
-                      <b>When/Scenario:</b> Performed in the same situations as
-                      typical layups, laybacks or reverse layups.
-                    </li>
-                  </ul>
-                </p>
-              </section>
+          </p>
+          <div class="cards">
+            <div class="card">
+              <img class="card__image" src={Booker} alt="" />
 
-              <section className="card2">
-                <h3>Inside Hand</h3>
-                <ul>
-                  <li>
-                    <b>How:</b>Player shoots with the closest hand to the
+              <div class="card__content">
+                <h2>Dunk</h2>
+                <p>
+                  <b>How:</b> Forcefully put the ball through the hoop off 1 or
+                  2 feet.
+                </p>
+                <p>
+                  <b>Why:</b> Is a better option over a typical layup if able to
+                  perform. A more aggressive play. Can create momentum or fouls
+                  depending on the situation.
+                </p>
+                <p>
+                  <b>When:</b> Performed in the same situations as typical
+                  layups, laybacks or reverse layups.
+                </p>
+              </div>
+            </div>
+            <div class="cards">
+              <div class="card">
+                <img class="card__image" src={Bron} alt="" />
+
+                <div class="card__content">
+                  <h2>Inside Hand</h2>
+                  <p>
+                    <b>How:</b> Player shoots with the closest hand to the
                     basket. Right handed shot when driving to the offensive
                     players left and Left handed when driving to the offensive
                     players right.
-                  </li>
-                  <li>
-                    <b>Why:</b> Why this Shot: Offensive player is driving and
-                    has gained a body position advantage and needs to shoot away
-                    from the defense so the shot is harder to block. Also, shot
-                    so that offensive player can use the rim as protection from
-                    a shot blocker.
-                  </li>
-                  <li>
+                  </p>
+                  <p>
+                    <b>Why:</b> Offensive player is driving and has gained a
+                    body position advantage and needs to shoot away from the
+                    defense so the shot is harder to block. Also, shot so that
+                    offensive player can use the rim as protection from a shot
+                    blocker.
+                  </p>
+                  <p>
                     <b>When:</b>Usually performed on a catch and sweep/rip
                     situation off a ball reversal or catch off a screen and
                     going “against the grain” on the drive and need to get the
                     shot up quickly before the guarding defender recovers or the
-                    help defender(s) rotate
-                  </li>
-                </ul>
-              </section>
+                    help defender(s) rotate.
+                  </p>
+                </div>
+              </div>
+              <div class="cards">
+                <div class="card">
+                  <img class="card__image" src={Bron} alt="" />
 
-              <section className="card3">
-                <h3>Jump/Baby Hook</h3>
-                <ul>
-                  <li>
-                    <b>How:</b>The off. player plants either foot and pivots
-                    over that same shoulder. Then, jumps off 2 feet keeping
-                    his/her body is perpendicular to the basket. Player
-                    completes the last half of the “hook shot” motion allowing
-                    for a quicker shot in the paint. Player uses off arm to
-                    create space for the shot and as protection from a shot
-                    blocker.
-                  </li>
-                  <li>
-                    <b>Why:</b>Quick way to score in the post are. Especially if
-                    wanting to score before being double teamed
-                  </li>
-                  <li>
-                    <b>When:</b>Mostly performed in a post up situation. Can
-                    also be performed when there is a drive to the basket and
-                    the driver passes to the player the rotating defender was
-                    guarding.
-                  </li>
-                </ul>
-              </section>
+                  <div class="card__content">
+                    <h2>Jump/Baby Hook</h2>
+                    <p>
+                      <b>How:</b>The off. player plants either foot and pivots
+                      over that same shoulder. Then, jumps off 2 feet keeping
+                      his/her body is perpendicular to the basket. Player
+                      completes the last half of the “hook shot” motion allowing
+                      for a quicker shot in the paint. Player uses off arm to
+                      create space for the shot and as protection from a shot
+                      blocker.
+                    </p>
+                    <p>
+                      <b>Why:</b> Quick way to score in the post are. Especially
+                      if wanting to score before being double teamed
+                    </p>
+                    <p>
+                      <b>When:</b> Mostly performed in a post up situation. Can
+                      also be performed when there is a drive to the basket and
+                      the driver passes to the player the rotating defender was
+                      guarding.
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
-          </p>
+          </div>
         </div>
       </div>
     </div>
